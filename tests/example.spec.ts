982ab2c430721bdcd64b02da3ba69ccc2effb89b
Andres Parra <andresparra1796@gmail.com>
2024-09-03 19:29:00 -0400

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,7 +8,7 @@ test('has title', async ({ page }) => {
 })
 
 test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/')
+    await page.goto('https://playwright.dev/    ')
 
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click()
